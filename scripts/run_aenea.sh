7dd2efa8ba866b785f55bd6da9b1284fab115f8f
El Autoestopista <elautoestopista@proyectopqmc.com>
2016-11-10 00:39:31 +0100

    Added start-stop script sysV-style :P

---
+++ scripts/run_aenea.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+#
+# Start and stop Aenea bot
+#
+
+AENEA_HOME=/srv/aenea/bot/aenea
+AENEA_BIN=aenea.py
+
+case "$1" in
+    start)
+        if [ -d $AENEA_HOME ] && [ -f $AENEA_HOME/$AENEA_BIN ]; then
+                nohup python3 $AENEA_HOME/$AENEA_BIN &
+        else
+                echo "ERROR: Aenea directory or binary not found"
+        fi
+      ;;
+    stop)
+        ps -ef | grep aenea.py | grep -v grep | tr -s " " | cut -d" " -f2 | xargs kill -9
+      ;;
+    status)
+        PID=`ps -ef | grep aenea.py | grep -v grep | tr -s " " | cut -d" " -f2`
+        if [ -z $PID ] ; then
+                echo Aenea not running
+        else
+                echo Aenea running on PID $PID
+        fi
+      ;;
+    *)
+      echo "Usage: $0 {start|stop|status}"
+      exit 1
+      ;;
+esac
